9bcffe9e03c801efc5a77237265dffff7c5e6678
Sergiusz Pu <serson85@gmail.com>
2020-04-08 14:59:18 +0200

    where is the problem?

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import OrderSummery from '../OrderSummery/OrderSummery';
 import {Row, Col} from 'react-flexbox-grid';
+import PropTypes from 'prop-types';
+import pricing from '../../../data/pricing.json';
+import OrderOption from '../OrderOption/OrderOption';
+
 
 const OrderForm = ({tripCost, options}) => (
   <Row>
+    <Col md={4}>
+      {pricing.map(type => (
+        <OrderOption key={type.id} {...type} />
+      ))}
+    </Col>
     <Col xs={12}>
       <OrderSummery tripCost={tripCost} options={options} />
     </Col>
   </Row>
 );
+
+OrderForm.propTypes = {
+  tripCost: PropTypes.string,
+  options: PropTypes.object,
+};
 
 export default OrderForm;
--- src/components/features/OrderSummery/OrderSummery.js
+++ src/components/features/OrderSummery/OrderSummery.js
@@ -2,13 +2,20 @@ import React from 'react';
 import styles from './OrderSummary.scss';
 import {calculateTotal} from '../../../utils/calculateTotal';
 import {formatPrice} from '../../../utils/formatPrice';
+import PropTypes from 'prop-types';
 
 
 const OrderSummary = ({tripCost, options}) => (
+
 
   <h2 className={styles.component}>
       Total {calculateTotal(formatPrice(tripCost), options)}
   </h2>
 );
+
+OrderSummary.propTypes = {
+  tripCost: PropTypes.string,
+  options: PropTypes.object,
+};
 
 export default OrderSummary;
