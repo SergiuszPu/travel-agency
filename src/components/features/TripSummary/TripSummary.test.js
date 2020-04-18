ba4d8093c1e13b7c30089bf9686c0e351b8296ba
Sergiusz Pu <serson85@gmail.com>
2020-04-18 14:35:24 +0200

    fix test inner tripssummary

--- src/components/features/TripSummary/TripSummary.test.js
+++ src/components/features/TripSummary/TripSummary.test.js
@@ -3,25 +3,29 @@ import {shallow} from 'enzyme';
 import TripSummary from './TripSummary';
 
 describe('Component TripSummary', () => {
-  it('should generate correct url link', () => {
+  it('should generate correct html link', () => {
     const id = 'abc';
     const component = shallow(<TripSummary id={id} />);
     const renderedLink = component.find('.link').prop('to');
     expect(renderedLink).toEqual(`/trip/${id}`);
     console.log(component.debug());
   });
-  
+
   it('should <img> to have correct src and alt', () => {
+    const id = 'abc';
     const expectedSrc = 'image';
     const expectedAlt = 'name';
-    const component = shallow(<TripSummary image={expectedSrc} name={expectedAlt} />);
+    const component = shallow(<TripSummary id={id} image={expectedSrc} name={expectedAlt} />);
 
     expect(component.find('img').prop('src')).toEqual(expectedSrc);
     expect(component.find('img').prop('alt')).toEqual(expectedAlt);
   });
 
   it('should render without crashing', () => {
-    const component = shallow(<TripSummary name={name} days={days} cost={cost}/>);
+    const id = 'abc';
+    const days = 3;
+    const cost = '123';
+    const component = shallow(<TripSummary id={id}  days={days} cost={cost}/>);
     expect(component).toBeTruthy();
   });
 
@@ -29,11 +33,24 @@ describe('Component TripSummary', () => {
     expect(() => shallow(<TripSummary />)).toThrow();
   });
 
+  it('should generate tags', () => {
+    const id = 'abc';
+    const tags = ['tags0', 'tags1'];
+    const wrapper = shallow(<TripSummary id={id} tags={tags} />);
+    const component = wrapper.find('.tags');
+    const spans = component.find('span');
 
-
-
-  it('index of tag is correct?', () => {
-    const wrapper = shallow(<TripSummary />);
-    expect(wrapper.find(tags).props().tags).toEqual('tags');
+    expect(spans.length).toEqual(tags.length);
+    expect(spans.at(0).text()).toEqual(tags[0]);
+    expect(spans.at(1).text()).toEqual(tags[1]);
   });
+
+  it('should not render tags element', () => {
+    const id ='abc';
+    const tags = [];
+    const wrapper = shallow(<TripSummary id={id} tags={tags} />);
+
+    expect(wrapper.contains('tags')).toEqual(false);
+  });
+  
 });
