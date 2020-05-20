f37e2ec8af6707fa19c34a98579d4f99ffdc5bb7
Sergiusz Pu <serson85@gmail.com>
2020-05-20 16:46:16 +0200

    add new descrition

--- src/components/features/HappyHourAd/HappyHourAd.js
+++ src/components/features/HappyHourAd/HappyHourAd.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import styles from './HappyHourAd.scss';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 
 
 class HappyHourAd extends React.Component {
+  constructor(){
+    super();
+
+    setInterval(() => {
+      this.forceUpdate();
+    }, 1000);
+
+  }
 
   getCountdownTime(){
-    const currentTime = new Date();
-    const nextNoon = new Date(Date.UTC(currentTime.getUTCFullYear(), currentTime.getUTCMonth(), currentTime.getUTCDate(), 12, 0, 0, 0));
+    const currentTime = new Date();//------>????????????????
+    const nextNoon = new Date(Date.UTC(currentTime.getUTCFullYear(), currentTime.getUTCMonth(), currentTime.getUTCDate(), 12, 0, 0, 0));//?????
   
     if(currentTime.getUTCHours() >= 12){
       nextNoon.setUTCDate(currentTime.getUTCDate()+1);
@@ -23,7 +31,7 @@ class HappyHourAd extends React.Component {
       <div>
         <h3 className={styles.title}> {title}</h3>
         <div className={styles.promoDescription}>{description}</div>
-        <div className='promoDescription'>{this.getCountdownTime()}</div>
+        <div className='promoDescription'>{this.getCountdownTime() > 23 }</div>
       </div>
     );
   }
